15df957d8a40638a8dda284d9b1fc4a3bcdac1ef
Adhyayan Bhandari <adhyayanbhandari@Adhyayans-MacBook-Pro.local>
2020-07-30 15:57:56 -0400

    Stock Header + Intro + Quotes

--- index.js
+++ index.js
@@ -1,1 +1,167 @@
+$('.div').hide();
+$('.high-a').hide();
+$('.high-d').hide();
+$('.low-d').hide();
+$('.low-a').hide();
+$('.Close').hide();
+$('.change').hide();
+$('.volume').hide();
+$('.ticker').hide();
+$('#stockquotes').css('padding-bottom','2%');
 
+
+
+$('.High-a').click(function(){
+  $('.high-a').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.div').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+
+})
+$('.Low-a').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.low-a').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.Ticker').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.ticker').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+})
+$('.Div').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.div').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.High-d').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.high-d').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.Low-d').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.low-d').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.closes').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.Close').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.change').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.Change').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.change').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.volume').hide();
+  $('.ticker').hide();
+})
+$('.Volume').click(function(){
+  $('#stockquotes').css('padding-bottom','5%');
+  $('.volume').fadeIn();
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
+  $('.div').hide();
+  $('.high-a').hide();
+  $('.high-d').hide();
+  $('.low-d').hide();
+  $('.low-a').hide();
+  $('.Close').hide();
+  $('.change').hide();
+  $('.ticker').hide();
+})
